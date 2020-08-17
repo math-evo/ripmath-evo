440574dafb8d00226b13828bbbe23991a6980fd8
Lingyong Sun <lysz210@gmail.com>
2020-08-17 21:42:10 +0200

    create basic katex component

---
+++ components/Katex.vue
@@ -0,0 +1,25 @@
+<template>
+  <span
+    ref="katex"
+    class="math formula latex"
+    v-html="formula"
+  ></span>
+</template>
+
+<script>
+import katex from 'katex'
+export default {
+  name: 'Katex',
+  data () {
+    return {
+      formula: "loading..."
+    }
+  },
+  mounted () {
+    let el = this.$slots.default.map(vnode => vnode.text).join("\n")
+    debugger
+    this.formula = katex.renderToString(el)
+    console.log(el, this.formula)
+  }
+}
+</script>
