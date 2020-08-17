0e8f6a3cf27841817e58a43a7e0429ce1e7bf7fd
Lingyong Sun <lysz210@gmail.com>
2020-08-17 21:42:33 +0200

    update formulas

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,7 +14,7 @@
       fixed
       app
     >
-      <v-toolbar-side-icon @click="drawer = !drawer" />
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
     </v-app-bar>
     <v-main>
@@ -29,14 +29,14 @@
       fixed
     >
       <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
             <v-icon light>
               compare_arrows
             </v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
       </v-list>
     </v-navigation-drawer>
     <v-footer
--- pages/index.vue
+++ pages/index.vue
@@ -45,11 +45,11 @@
         <v-card-text>
           <p>
             Questo sito e' pensato per chi ha bisogno di recuperare in matematica, pertanto non ci troverai grandi costruzioni logiche, ma cose semplici e spiegate nella maniera piu' semplice che l'esperienza mi ha suggerito; talvolta qualche costrutto logico non sara' proprio ortodosso ma servira' a capire meglio il concetto.
-            <br />
+            <br>
             Una volta capito il concetto vedrai che la matematica e' facile ed anche piacevole.
-            <br />
+            <br>
             Pero' dovrai avere tanta pazienza e non stancarti di cercare di capire: ho detto cercare di capire non di fare gli esercizi, perche' l'esercizio deve servire solo a fissare meglio quello che si e' capito e non deve essere fine a se' stesso.
-            <br />
+            <br>
             Quando si e' capito il concetto e' facile risolvere l'esercizio.
           </p>
           <p>
@@ -57,7 +57,7 @@
           </p>
           <p>
             La scimmia e' un animale che impara ad usare un oggetto per uno scopo, ad esempio un bastone per raggiungere una banana ma se cambi il tipo di bastone e le dai un bastone diviso in due che deve montare di solito la scimmia non e' piu' capace di raggiungere la banana.
-            <br />
+            <br>
             Io avevo una gatta che aveva imparato ad aprire la porta di casa saltando sulla maniglia e cosi' facendo scattare la serratura la porta si apriva; ma questo riusciva a farlo solo con la porta di casa; a 3 metri dalla porta di casa c'e' la porta della cantina che e' identica a quella di casa, ma la gatta non ha mai provato ad aprire la porta della cantina: cio' che voglio dire e' che le bestie non sono capaci di generalizzare (per fortuna della mia cantina), mentre gli esseri umani si'; abbiamo un cervello che e' una macchina  meravigliosa, impariamo ad usarlo al meglio. invece di fare le cose meccanicamente.
           </p>
           <p>
@@ -68,7 +68,7 @@
           </p>
           <p>
             Intendiamoci: alcuni esercizi bisogna darli, sia per fissare meglio i concetti della teoria, sia per acquistare manualita' e velocita' di esecuzione.
-            <br />
+            <br>
             Quanti farne? Questo e' un problema: io di solito suggerisco di leggere l'esercizio, se sei in grado leggendo l'esercizio di capire come devi iniziare, come devi procedere, se ci sono delle particolarita' ed infine come devi concludere, secondo me non e' necessario fare quell'esercizio, mentre se trovi un esercizio in cui vedi che sei capace di iniziare, ma trovi delle particolarita' che non avevi mai visto allora quello e' il caso di risolverlo.
           </p>
           <p>
@@ -78,33 +78,47 @@
             A tal proposito bisogna sempre distinguere fra l'esercizio che deriva dalla teoria e quelle che io chiamo le sovrastrutture: mi spiego con un esempio: se io ti dico  2a+3a= tu devi sommare due cose uguali con altre 3 cose uguali che fanno 5 cose uguali scrivi 5a.
           </p>
           <p>
-            Ma se io ti dico  2Log5+3Log5 cosa scrivi? evidentemente 5Log5 perche' anche queste sono 2 cose uguali piu' altre 3 cose uguali allora Log5 e' quello che io chiamo sovrastruttura; e guarda che posso renderla complicata quanto voglio.
+            Ma se io ti dico <katex>2\log 5 +3\log 5</katex> cosa scrivi? evidentemente <katex>5\log 5</katex> perche' anche queste sono 2 cose uguali piu' altre 3 cose uguali allora <katex>\log 5</katex> e' quello che io chiamo sovrastruttura; e guarda che posso renderla complicata quanto voglio.
           </p>
           <p>
-            2(a&#178;b&#179;+Log5-7x&#179;y&#170;)+3(a&#178;b&#179;+Log5-7x&#179;y&#170;)=5(a&#178;b&#179;+Log5-7x&#179;y&#170;)
+            <katex>
+              2(a^2 b^3 +\log 5 -7x^3 y^a) +3(a^2 b^3 + \log 5 - 7x^3 y^a) = \newline
+              5(a^2 b^3 +\log 5 -7x^3 y^a)
+            </katex>
           </p>
           <p>
-            Se sarai capace di distinguere le regole dalle sovrastrutture vedrai che tutto sara' piu' facile<p> Purtroppo pero' non e' una cosa semplice: ogni anno davo un esercizio in classe ai miei alunni di questo tipo:
-          <p>
-          ( 2x-y)&#179;-3(2x-y)&#178;(2x+y)+3(2x-y)(2x+y)&#178;-(2x+y)&#179;= -8y&#179; se lo osservi bene potrai osservare che ha la struttura del cubo di un binomio<p> a&#179;-3a&#178;b+3ab&#178;-b&#179;=(a-b)&#179;
+            Se sarai capace di distinguere le regole dalle sovrastrutture vedrai che tutto sara' piu' facile.
+            <br>
+            Purtroppo pero' non e' una cosa semplice: ogni anno davo un esercizio in classe ai miei alunni di questo tipo:
+            <br>
+            <katex>
+              (2x -y)^3 -3(2x -y)^2 (2x+y) +3(2x -y)(2x +y)^2 -(2x +y)^3 = -8y^3
+            </katex>
+            <br>
+            se lo osservi bene potrai osservare che ha la struttura del cubo di un binomio
+            <br>
+            <katex>
+              a^3 -3a^2 b +3ab^2 -b^3 = (a -b)^3 
+            </katex>
           </p>
           <p>
             in tanti anni di insegnamento nessuno dei miei alunni ha pensato di risolverlo senza fare tutti i calcoli.
           </p>
           <p>
-          Come si potrebbe fare? cosi'
-          </p>
-          <p>
-            (2x-y)&#179;-3(2x-y)&#178;(2x+y)+3(2x-y)(2x+y)&#178;-(2x+y)&#179;=<p>=[(2x-y)-(2x+y)]&#179;=
-            <br />
-            =[2x-y-2x-y]&#179;=<p>=(-2y)&#179;=
-            <br />
-            =-8y&#179;
-            <br />
+            Come si potrebbe fare? cosi'
+            <br>
+            <katex>
+              (2x -y)^3 -3(2x -y)^2 (2x +y) +3(2x -y)(2x +y)^2 -(2x +y)^3 = \newline
+              =[(2x -y)-(2x +y)]^3 = \newline
+              =[2x -y -2x -y]^3 = \newline
+              =(-2y)^3 = \newline
+              =-8y^3
+            </katex>
+            <br>
             ma evidentemente per farlo occorre aver molta dimestichezza con le sovrastrutture.
-            <br />
+            <br>
             Ammetterai comunque che questo metodo e' piu' "elegante" che il fare tutti i calcoli  anche se il risultato e' lo stesso.
-            <br />
+            <br>
             Vedere fra vari metodi di soluzione un esercizio svolto in modo elegante da' effettivamente piacere
           </p>
           <p>
@@ -118,3 +132,13 @@
     </v-flex>
   </v-layout>
 </template>
+
+<script>
+import Katex from '~/components/Katex'
+export default {
+  name: 'Index',
+  components: {
+    Katex
+  }
+}
+</script>
